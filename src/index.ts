6fd236f170640b4272262e1101a7a780876c3993
D0dii <marcindolatowski123@gmail.com>
2025-06-16 20:01:17 +0200

    feat: move people list to json

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client, GatewayIntentBits, TextChannel } from "discord.js";
 import "dotenv/config";
 import { notifyAboutLastUserMatch } from "./utils";
 import { setupDb } from "./db";
+import people from "../people.json";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -9,24 +10,13 @@ const client = new Client({
 
 const ritoToken = process.env.RIOT_TOKEN ?? "";
 const channelId = process.env.CHANNEL_ID ?? "";
-const moiPrzyjaciele = [
-  {
-    username: "minkii",
-    tag: "eune",
-    game: "lol",
-  },
-  { username: "lorekk", tag: "mucha", game: "lol" },
-  { username: "sledzionaaa", tag: "eune", game: "lol" },
-  { username: "dodi", tag: "goat", game: "lol" },
-  { username: "ak123133", tag: "CANT", game: "lol" },
-];
 client.once("ready", async () => {
   console.log(`Zalogowano jako ${client.user?.tag}`);
   const channel = await client.channels.fetch(channelId);
   const db = await setupDb();
 
   if (channel && channel instanceof TextChannel) {
-    for (const user of moiPrzyjaciele) {
+    for (const user of people) {
       if (user.game === "lol") {
         await notifyAboutLastUserMatch(user.username, user.tag, ritoToken, channel);
       }
